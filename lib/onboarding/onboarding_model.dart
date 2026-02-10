1b4b56d248a2d55a6a7b78eaee21b282209d03ce
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-02-10 04:11:24 +0000

    Updating to latest FlutterFlow output.

--- lib/onboarding/onboarding_model.dart
+++ lib/onboarding/onboarding_model.dart
@@ -18,6 +18,10 @@ class OnboardingModel extends FlutterFlowModel<OnboardingWidget> {
   FocusNode? textFieldFocusNode2;
   TextEditingController? textController2;
   String? Function(BuildContext, String?)? textController2Validator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode3;
+  TextEditingController? textController3;
+  String? Function(BuildContext, String?)? textController3Validator;
   DateTime? datePicked;
 
   @override
@@ -30,5 +34,8 @@ class OnboardingModel extends FlutterFlowModel<OnboardingWidget> {
 
     textFieldFocusNode2?.dispose();
     textController2?.dispose();
+
+    textFieldFocusNode3?.dispose();
+    textController3?.dispose();
   }
 }
